ad9cce9bb3a9a86884c3e1d00c932dc04235a6a3
baardl <bard.lind@gmail.com>
2023-10-19 15:15:02 +0200

    Project setup for Demo

--- run-all.sh
+++ run-all.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
+mkdir -p bin
+mkdir -p plugins
 rm -rf bin/*
 rm -rf plugins/*
 mvn clean install
-cp plugin-typelib/target/*.jar bin
-cp plugin-application/target/*.jar bin
 
 echo '*'
 echo '*'
 echo '* Build is done, now run'
 echo '*'
 echo '*'
-
+echo Copy runtime jars to \"bin\" csv plugin to \"plugins\"
+cp plugin-typelib/target/*.jar bin
+cp plugin-application/target/*.jar bin
+echo '*'
 echo Run with simulator only
 java -cp ./bin/*:./plugins/* no.baardl.plugins.App
 echo '*'
-echo Add CSV plugin and re-run
+echo Add CSV plugin to \"plugins\" and re-run
 cp csv-plugin/target/*.jar plugins
 java -cp ./bin/*:./plugins/* no.baardl.plugins.App
